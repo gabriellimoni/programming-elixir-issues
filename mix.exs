46d6a6e6090702e62178f93778ee71803375512d
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-08-09 08:34:51 -0300

    docs: add docs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Issues.MixProject do
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.13",
+      name: "Issues",
+      source_url: "https://github.com/gabriellimoni/programming-elixir-issues",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,9 +25,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       { :httpoison, "~> 1.8.1" },
-      { :poison, "~> 5.0" }
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :poison, "~> 5.0" },
+      { :ex_doc, "~> 0.28.4"},
+      { :earmark, "~> 1.4.26"}
     ]
   end
 
